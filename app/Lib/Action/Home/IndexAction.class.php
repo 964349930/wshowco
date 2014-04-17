7fa222ebe3fdb84eff11f97bbfc443be634e7c60
ChanMo <orangechen.1991@gmail.com>
2014-04-17 22:49:09 +0800

    orange tpl

--- app/Lib/Action/Home/IndexAction.class.php
+++ app/Lib/Action/Home/IndexAction.class.php
@@ -14,37 +14,5 @@ class IndexAction extends HomeAction {
         $this->display();
     }
 
-   /**
-     * 头部菜单
-     */
-    protected function _getChannel() {
-		$arrList = array();
-		$tabList = D('Tab')->getTabList();
-		foreach($tabList as $k=>$v){
-			$arrList[$v['tag']] = $v['title'];
-		}
-		return $arrList;
-	}
 
-    /**
-     * 左侧菜单
-     */
-    protected function _getMenu() {
-        $menu = array();
-		$first_list = D('Tab')->getTabList();
-		foreach($first_list as $k=>$v){
-			$second_list = D('Tab')->getTabList($v['id']);	
-			foreach($second_list as $k2=>$v2){
-				$third_list = D('Tab')->getTabList($v2['id']);
-				foreach($third_list as $k3=>$v3){
-					$third_real_list[$v3['title']] = U($v3['url']);
-					$second_real_list[$v2['title']] = $third_real_list;
-				}
-				$third_real_list = array();
-			}
-			$menu[$v['tag']] = $second_real_list;
-			$second_real_list = array();
-		}
-        return $menu;
-    }
 }
--- app/Lib/Action/HomeAction.class.php
+++ app/Lib/Action/HomeAction.class.php
@@ -14,6 +14,8 @@ class HomeAction extends BaseAction
         if(!isset($_SESSION['uid'])){
             $this->redirect('Public/login');
         }
+        $this->assign('channel', $this->_getChannel());
+        $this->assign('menu',    $this->_getMenu());
         $this->checkLoa();
     }
 
@@ -34,5 +36,51 @@ class HomeAction extends BaseAction
                 exit;
             }
         }
+    }
+   /**
+     * 头部菜单
+     */
+    protected function _getChannel() {
+        $arrList = array();
+        $tabList = D('Tab')->getTabList();
+        foreach($tabList as $k=>$v){
+            $arrList[$v['tag']] = $v['title'];
+        }
+        return $arrList;
+    }
+
+    /**
+     * 左侧菜单
+     */
+    protected function _getMenu() {
+        $tabObj = D('Tab');
+        $menu = $tabObj->getTabList();
+        foreach($menu as $k=>$v){
+            $menu[$k]['menu'] = $tabObj->getTabList($v['id']);
+            foreach($menu[$k]['menu'] as $k2=>$v2){
+                $menu[$k]['menu'][$k2]['meta'] = $tabObj->getTabList($v2['id']);
+                foreach($menu[$k]['menu'][$k2]['meta'] as $k3=>$v3){
+                    $menu[$k]['menu'][$k2]['meta'][$k3]['url'] = U($v3['url']);
+                }
+            }
+        }
+        return $menu;
+        /*
+        $first_list = D('Tab')->getTabList();
+        foreach($first_list as $k=>$v){
+            $second_list = D('Tab')->getTabList($v['id']);
+            foreach($second_list as $k2=>$v2){
+                $third_list = D('Tab')->getTabList($v2['id']);
+                foreach($third_list as $k3=>$v3){
+                    $third_real_list[$v3['title']] = U($v3['url']);
+                    $second_real_list[$v2['title']] = $third_real_list;
+                    }
+                $third_real_list = array();
+            }
+            $menu[$v['tag']] = $second_real_list;
+            $second_real_list = array();
+        }
+        return $menu;
+        */
     }
 }
